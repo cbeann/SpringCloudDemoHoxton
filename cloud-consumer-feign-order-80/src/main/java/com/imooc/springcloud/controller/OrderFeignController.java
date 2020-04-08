839139b3edbee4bc2c182c41cb0cd03600c7db77
cbeann <cbeann@163.com>
2020-04-08 22:43:41 +0800

    cloud-consumer-feign-order-80

---
+++ cloud-consumer-feign-order-80/src/main/java/com/imooc/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,25 @@
+package com.imooc.springcloud.controller;
+
+import com.imooc.springcloud.entities.CommonResult;
+import com.imooc.springcloud.entities.Payment;
+import com.imooc.springcloud.service.PaymentFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author CBeann
+ * @create 2020-04-08 22:26
+ */
+@RestController
+public class OrderFeignController {
+
+    @Autowired
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        return paymentFeignService.getPaymentById(id);
+    }
+}
---
+++ cloud-consumer-feign-order-80/src/main/java/com/imooc/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,20 @@
+package com.imooc.springcloud.service;
+
+import com.imooc.springcloud.entities.CommonResult;
+import com.imooc.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author CBeann
+ * @create 2020-04-08 22:11
+ */
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-PAYMENT")
+public interface PaymentFeignService {
+
+    @GetMapping(value = "/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+}
