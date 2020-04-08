e7e2210a5853748a826f7a38445d4961c4fb0de8
cbeann <cbeann@163.com>
2020-04-08 18:37:44 +0800

    ZK服务提供者与服务消费者

---
+++ cloud-consumerzk-order-80/src/main/java/com/imooc/springcloud/controller/OrderZkController.java
@@ -0,0 +1,28 @@
+package com.imooc.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author CBeann
+ * @create 2020-04-08 11:35
+ */
+@RestController
+@Slf4j
+public class OrderZkController {
+    private static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/zk")
+    public String paymentInfo(){
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+        return result;
+    }
+
+
+}
