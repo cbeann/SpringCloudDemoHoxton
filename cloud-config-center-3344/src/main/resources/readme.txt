c5df4fe31db39a2a5269cbe96d34ce4054a96f5b
cbeann <cbeann@163.com>
2020-04-10 01:52:54 +0800

    bus

---
+++ cloud-config-center-3344/src/main/resources/readme.txt
@@ -0,0 +1,2 @@
+curl -X POST "http://localhost:3344/actuator/bus-refresh"
+curl -X POST "http://localhost:3344/actuator/bus-refresh/config-client:3355"
---
+++ cloud-config-client-3366/src/main/java/com/imooc/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.imooc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author CBeann
+ * @create 2020-04-10 1:22
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
