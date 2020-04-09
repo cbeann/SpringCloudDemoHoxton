dd7227f60ddb7cdd88b00e96d74284aab3c6f241
cbeann <cbeann@163.com>
2020-04-09 18:00:40 +0800

    config-center-3344 config-client-3355

---
+++ cloud-config-center-3344/src/main/java/com/imooc/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.imooc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author CBeann
+ * @create 2020-04-09 17:12
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/imooc/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.imooc.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author CBeann
+ * @create 2020-04-09 17:34
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+
+}
